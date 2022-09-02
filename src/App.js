be17b4f98a28822c3c6362b51ec9a6255c30a6ac
Saiful Islam <57891501+prosaiful37@users.noreply.github.com>
2022-09-02 10:25:15 +0600

    initial setup for website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Shared/Header/Header';
+import Footer from './Components/Shared/Footer/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import Blogs from './Components/Blog/Blogs';
+import Login from './Components/Login/Login/Login';
+import Register from './Components/Login/Register/Register';
+import Contact from './Components/Contact us/Contact';
+import NotFound from './Components/Shared/NotFound/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/blog' element={<Blogs></Blogs>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
